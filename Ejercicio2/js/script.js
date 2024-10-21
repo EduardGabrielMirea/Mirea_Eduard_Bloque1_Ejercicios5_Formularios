e644878ad3303a540aa933fdbb9564bc7b219637
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-10-21 12:40:00 +0200

    commit

--- Ejercicio2/js/script.js
+++ Ejercicio2/js/script.js
@@ -1,12 +1,15 @@
-document.addEventListener("DOMContentLoaded",function () {
-    
+document.addEventListener("DOMContentLoaded", function () {
+    const selectColores = document.getElementById('colores');
 
+    selectColores.addEventListener('change', function() {
+        cambiarColor();
+    });
+    
     // Función para cambiar el color del párrafo
     function cambiarColor() {
         const parrafo = document.getElementById('parrafo');
         const colorSeleccionado = document.getElementById('colores').value;
 
-        // Aplicar el color seleccionado
         if (colorSeleccionado === 'verde') {
             parrafo.style.color = 'green';
         } else if (colorSeleccionado === 'azul') {
